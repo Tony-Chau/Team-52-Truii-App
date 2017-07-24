135c052b11af8b0e312490424947cf2576673fec
Tony Chau <tonychau923@gmail.com>
2017-07-24 13:40:34 +1000

    Each buttons can now open a new page

---
+++ Administration App/Administration App/DatabaseActivity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Administration_App
+{
+    [Activity(Label = "DatabaseActivity")]
+    public class DatabaseActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.DatabaseView);
+            // Create your application here
+            string databaseName = Intent.GetStringExtra("DatabaseName");
+            if (databaseName == "User"){
+                OperateUser();
+            }
+            else if(databaseName == "Graph"){
+                OperateGraph();
+            }
+            else if(databaseName == "Field"){
+                OperateField();
+            }
+            else if(databaseName == "Table"){
+                OperateTable();
+            }
+            else{
+                OperateVarious(databaseName);
+            }
+        }
+        private void OperateUser()
+        {
+
+        }
+
+        private void OperateGraph()
+        {
+
+        }
+        
+        private void OperateTable()
+        {
+
+        }
+
+        private void OperateField()
+        {
+
+        }
+
+        private void OperateVarious(string TableName)
+        {
+
+        }
+    }
+}
--- Administration App/Administration App/MainActivity.cs
+++ Administration App/Administration App/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace Administration_App
 {
@@ -23,8 +24,37 @@ namespace Administration_App
             btnGraph = FindViewById<Button>(Resource.Id.btnGraph);
             btnField = FindViewById<Button>(Resource.Id.btnField);
             btnVarious = FindViewById<Button>(Resource.Id.btnVarious);
+            btnUser.Click += BtnUser_Click;
+            btnTable.Click += BtnTable_Click;
+            btnGraph.Click += BtnGraph_Click;
+            btnField.Click += BtnField_Click;
         }
-        
+
+        private void BtnField_Click(object sender, System.EventArgs e)
+        {
+            NextPage("Field");
+        }
+
+        private void BtnGraph_Click(object sender, System.EventArgs e)
+        {
+            NextPage("Graph");
+        }
+
+        private void BtnTable_Click(object sender, System.EventArgs e)
+        {
+            NextPage("Table");
+        }
+
+        private void BtnUser_Click(object sender, System.EventArgs e)
+        {
+            NextPage("User");
+        }
+        private void NextPage(string DatabaseName)
+        {
+            Intent intent = new Intent(this, typeof(DatabaseActivity));
+            intent.PutExtra("DatabaseName", DatabaseName);
+            StartActivity(intent);
+        }
     }
 }
 
