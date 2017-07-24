f25c89b0caac9581e37d0e28e2d29e6ba3091375
Tony Chau <tonychau923@gmail.com>
2017-07-24 12:54:00 +1000

    Created the buttons and assigned them

--- Administration App/Administration App/MainActivity.cs
+++ Administration App/Administration App/MainActivity.cs
@@ -7,13 +7,24 @@ namespace Administration_App
     [Activity(Label = "Administration_App", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        Button btnUser;
+        Button btnTable;
+        Button btnGraph;
+        Button btnField;
+        Button btnVarious;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+            btnUser = FindViewById<Button>(Resource.Id.btnUser);
+            btnTable = FindViewById<Button>(Resource.Id.btnTable);
+            btnGraph = FindViewById<Button>(Resource.Id.btnGraph);
+            btnField = FindViewById<Button>(Resource.Id.btnField);
+            btnVarious = FindViewById<Button>(Resource.Id.btnVarious);
         }
+        
     }
 }
 
--- Administration App/Administration App/Resources/Resource.Designer.cs
+++ Administration App/Administration App/Resources/Resource.Designer.cs
@@ -57,6 +57,34 @@ namespace Administration_App
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050003
+			public const int btnField = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int btnGraph = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int btnTable = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int btnUser = 2131034112;
+			
+			// aapt resource value: 0x7f050004
+			public const int btnVarious = 2131034116;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
